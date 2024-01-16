8f4e0a3710b5abd7e9492e16b4b4a0450df91a5e
nsnisarga <nisarganayak02@gmail.com>
2024-01-16 11:37:34 +0530

    commit123

--- program1.py
+++ program1.py
@@ -4,10 +4,10 @@ def fibonacciSeries(i):
  else:
     return (fibonacciSeries(i - 1) + fibonacciSeries(i - 2))
 
-num=int(input("enter the number of terms you want??"))
-if num <=0:
+n=int(input("enter the number of terms you want??"))
+if n <=0:
   print("Please enter a Positive Number")
 else:
   print("Fibonacci Series:", end=" ")
-for i in range(num):
+for i in range(n):
   print(fibonacciSeries(i), end=" ")
